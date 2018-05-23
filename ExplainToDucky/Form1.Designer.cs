64b3b4191bc962f875807c839223e6962f564827
TF <tomas@florian.ca>
2018-05-23 17:00:50 -0600

    Add project files.

---
+++ ExplainToDucky/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ExplainToDucky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonSmallerFont = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonLargerFont = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSmallerFont
+            // 
+            this.buttonSmallerFont.Location = new System.Drawing.Point(3, 3);
+            this.buttonSmallerFont.Name = "buttonSmallerFont";
+            this.buttonSmallerFont.Size = new System.Drawing.Size(87, 28);
+            this.buttonSmallerFont.TabIndex = 1;
+            this.buttonSmallerFont.Text = "Smaller Font";
+            this.buttonSmallerFont.UseVisualStyleBackColor = true;
+            this.buttonSmallerFont.Click += new System.EventHandler(this.buttonSmallerFont_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 183F));
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(794, 403);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonLargerFont);
+            this.panel1.Controls.Add(this.buttonSmallerFont);
+            this.panel1.Location = new System.Drawing.Point(3, 412);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(316, 35);
+            this.panel1.TabIndex = 2;
+            // 
+            // buttonLargerFont
+            // 
+            this.buttonLargerFont.Location = new System.Drawing.Point(96, 3);
+            this.buttonLargerFont.Name = "buttonLargerFont";
+            this.buttonLargerFont.Size = new System.Drawing.Size(87, 28);
+            this.buttonLargerFont.TabIndex = 2;
+            this.buttonLargerFont.Text = "Larger Font";
+            this.buttonLargerFont.UseVisualStyleBackColor = true;
+            this.buttonLargerFont.Click += new System.EventHandler(this.buttonLargerFont_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Duck";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSmallerFont;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonLargerFont;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ExplainToDucky/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExplainToDucky
+{
+    public partial class Form1 : Form
+    {
+        
+        private int idleSeconds = 0;
+        private int maxGrey = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSmallerFont_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Font.Size > 8)
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, richTextBox1.Font.Size - 1);
+            }
+            else
+            {
+                maxGrey += 20;
+            }
+            
+        }
+
+        private void buttonLargerFont_Click(object sender, EventArgs e)
+        {
+            maxGrey = 0;
+
+            richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, richTextBox1.Font.Size + 1);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (richTextBox1.Text.Length > 600)
+            {
+                int selectionStart = richTextBox1.SelectionStart;
+
+                richTextBox1.Text = richTextBox1.Text.Substring(300);
+
+                richTextBox1.Select(selectionStart, 0);
+            }
+            
+            idleSeconds++;
+
+
+            int greyTone = maxGrey + (idleSeconds * 5);
+            if(greyTone > 255)
+            {
+                greyTone = 255;
+            }
+
+                
+            richTextBox1.ForeColor = Color.FromArgb(greyTone, greyTone, greyTone);
+                
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //idleSeconds = 0;
+        }
+
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            idleSeconds = 0;
+        }
+    }
+}
